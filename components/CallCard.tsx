fea11078f0d9410f8a3862aaf38e45cc5006bca7
morshaked <mor.s@mykey.co.il>
2024-04-09 12:39:37 +0300

    new

--- components/CallCard.tsx
+++ components/CallCard.tsx
@@ -1,18 +1,95 @@
-import { Call, CallRecording } from "@stream-io/video-react-sdk";
-import React from "react";
-import { FaCalendarAlt } from "react-icons/fa";
+"use client";
+
+import Image from "next/image";
+
+import { cn } from "@/lib/utils";
+import { Button } from "./ui/button";
+import { useToast } from "./ui/use-toast";
+import { ReactElement } from "react";
+import { CopyIcon } from "lucide-react";
+import { avatarImages } from "@/constants";
 
 interface CallCardProps {
-  meeting: Call | CallRecording;
-  type: "upcoming" | "previous" | "recordings";
+  title: string;
+  date: string;
+  icon: ReactElement<any, any>;
+  isPreviousMeeting?: boolean;
+  buttonIcon1?: ReactElement<any, any> | undefined;
+  buttonText?: string;
+  handleClick: () => void;
+  link: string;
 }
-const CallCard = ({ meeting, type }: CallCardProps) => {
-  console.log(meeting);
+
+const CallCard = ({
+  icon,
+  title,
+  date,
+  isPreviousMeeting,
+  buttonIcon1,
+  handleClick,
+  link,
+  buttonText,
+}: CallCardProps) => {
+  const { toast } = useToast();
+
   return (
-    <div className="bg-dark-3 w-full flex rounded-[14px] px-6 py-8 flex-col">
-      <FaCalendarAlt className="h-6 w-6 text-white" />
-      <h2></h2>
-    </div>
+    <section className="flex min-h-[258px] w-full flex-col justify-between rounded-[14px] bg-dark-1 px-5 py-8 xl:max-w-[568px]">
+      <article className="flex flex-col gap-5">
+        {icon}
+        <div className="flex justify-between">
+          <div className="flex flex-col gap-2">
+            <h1 className="text-2xl font-bold">{title}</h1>
+            <p className="text-base font-normal">{date}</p>
+          </div>
+        </div>
+      </article>
+      <article className={cn("flex justify-center relative", {})}>
+        <div className="relative flex w-full max-sm:hidden">
+          {avatarImages.map((img, index) => (
+            <Image
+              key={index}
+              src={img}
+              alt="attendees"
+              width={40}
+              height={40}
+              className={cn("rounded-full", { absolute: index > 0 })}
+              style={{ top: 0, left: index * 28 }}
+            />
+          ))}
+          <div className="flex-center absolute left-[136px] size-10 rounded-full border-[5px] border-dark-3 bg-dark-4">
+            +5
+          </div>
+        </div>
+        {!isPreviousMeeting && (
+          <div className="flex gap-2">
+            <Button
+              onClick={handleClick}
+              className="rounded bg-blue-1 px-6 text-center"
+            >
+              {buttonIcon1 && (
+                <>
+                  {buttonIcon1}
+                  &nbsp;
+                </>
+              )}
+              {buttonText}
+            </Button>
+            <Button
+              onClick={() => {
+                navigator.clipboard.writeText(link);
+                toast({
+                  title: "Link Copied",
+                });
+              }}
+              className="bg-dark-4 px-6"
+            >
+              <CopyIcon size={20} />
+              &nbsp; Copy Link
+            </Button>
+          </div>
+        )}
+      </article>
+    </section>
   );
 };
 
--- components/CallList.tsx
+++ components/CallList.tsx
@@ -5,6 +5,8 @@ import { Call, CallRecording } from "@stream-io/video-react-sdk";
 import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 import CallCard from "./CallCard";
+import { FaCalendarAlt, FaFolderOpen, FaVideo } from "react-icons/fa";
+import { PlayIcon } from "lucide-react";
 
 interface CallListProps {
   type: "upcoming" | "previous" | "recordings";
@@ -48,7 +50,42 @@ const CallList = ({ type }: CallListProps) => {
     <div className="grid grid-cols-1 gap-5 xl:grid-cols-2">
       {calls && calls.length > 0 ? (
         calls.map((meeting: Call | CallRecording, i) => (
-          <CallCard key={i} meeting={meeting} type={type} />
+          <CallCard
+            key={(meeting as Call).id}
+            icon={
+              type === "previous" ? (
+                <FaFolderOpen className="h-6 w-6" />
+              ) : type === "upcoming" ? (
+                <FaCalendarAlt className="h-6 w-6" />
+              ) : (
+                <FaVideo className="h-6 w-6" />
+              )
+            }
+            title={
+              (meeting as Call).state?.custom?.description ||
+              (meeting as CallRecording).filename?.substring(0, 20) ||
+              "No Description"
+            }
+            date={
+              (meeting as Call).state?.startsAt?.toLocaleString() ||
+              (meeting as CallRecording).start_time?.toLocaleString()
+            }
+            isPreviousMeeting={type === "previous"}
+            link={
+              type === "recordings"
+                ? (meeting as CallRecording).url
+                : `${process.env.NEXT_PUBLIC_BASE_URL}/meeting/${
+                    (meeting as Call).id
+                  }`
+            }
+            buttonIcon1={type === "recordings" ? <PlayIcon /> : undefined}
+            buttonText={type === "recordings" ? "Play" : "Start"}
+            handleClick={
+              type === "recordings"
+                ? () => router.push(`${(meeting as CallRecording).url}`)
+                : () => router.push(`/meeting/${(meeting as Call).id}`)
+            }
+          />
         ))
       ) : (
         <h1>{noCallsMessage}</h1>
